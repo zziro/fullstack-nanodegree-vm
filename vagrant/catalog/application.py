1ae02c23e99818e5b409e31c464674447c3435a4
zziro <zziro.123@gmail.com>
2018-05-24 16:50:48 -0500

    Secret-key

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -221,5 +221,6 @@ def deleteCategoryItemDetail(category_id, item_id):
 
 
 if __name__ == '__main__':
-    app.debug = True    
+    app.debug = True
+    #app.secret_key = 'super_secret_key'
     app.run(host='0.0.0.0', port=5000)
